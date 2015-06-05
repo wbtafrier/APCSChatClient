f09ec6d0923b1e0dee9b2f669428f5c816fa8e50
Andy608 <AndrewR608@gmail.com>
2015-06-05 15:49:25 -0400

    Added up-to-date jars.

--- src/com/compsci/gui/ClientFrame.java
+++ src/com/compsci/gui/ClientFrame.java
@@ -4,6 +4,7 @@ import java.awt.Dimension;
 import java.awt.event.WindowAdapter;
 import java.awt.event.WindowEvent;
 
+import javax.swing.ImageIcon;
 import javax.swing.JFrame;
 
 import com.compsci.core.SloverseClient;
