7430521108d5580419e37a97f94f35e1005d84b4
ISQUISHALL <wbtafrier@gmail.com>
2015-06-15 14:35:45 -0400

    A bit more work on files... kinda just want this done so next commit will most likely be huge

--- src/com/compsci/file/Directory.java
+++ src/com/compsci/file/Directory.java
@@ -1,5 +1,27 @@
 package com.compsci.file;
 
+import java.io.File;
+
 public class Directory {
 
+	public Directory(String path) {
+		
+	}
+	
+	public File toFile() {
+		return null;
+	}
+	
+	public Directory up() {
+		return null;
+	}
+	
+	public Directory down(String folderName) {
+		return null;
+	}
+	
+	public File[] getFiles() {
+		return null;
+	}
+	
 }
--- src/com/compsci/file/FileManager.java
+++ src/com/compsci/file/FileManager.java
@@ -2,10 +2,26 @@ package com.compsci.file;
 
 import java.io.File;
 
-public class FileManager {
+public final class FileManager {
 
+	private static final String s = File.separator;
+	private static Directory sloverseDirectory;
+	
 	public static void initalizeStoragePath() {
-		
+		String operatingSystem = System.getProperty("os.name").toLowerCase();
+		if (operatingSystem.contains("win")) {
+			//Windows
+			String appData = System.getenv("APPDATA");
+			sloverseDirectory = new Directory(appData);
+		}
+		else if (operatingSystem.contains("os x")) {
+			//Mac
+			String appSupport = System.getProperty("user.home") + s + "Library" + s + "Application Support";
+		}
+		else {
+			//Linux, etc.
+			String home = System.getProperty("user.home");
+		}
 	}
 	
 	public static File retrieveFile(Directory d, String fileName) {
